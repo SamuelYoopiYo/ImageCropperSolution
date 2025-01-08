6a14170216b416187f089d1b37d6a69cfe576573
SamuelYoopiYo <smnvsrg@yandex.ru>
2025-01-08 13:39:26 +0300

    create ImageCropperMAUI

---
+++ ImageCropperMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ImageCropperMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ImageCropperMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageCropperMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
