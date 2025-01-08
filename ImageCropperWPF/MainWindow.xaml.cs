8695e85259dade1fef5f2aed256bd655d14d5907
SamuelYoopiYo <smnvsrg@yandex.ru>
2025-01-08 13:12:24 +0300

    add template xaml and cs code, add open image in WPF

--- ImageCropperWPF/MainWindow.xaml.cs
+++ ImageCropperWPF/MainWindow.xaml.cs
@@ -8,17 +8,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ImageCropperLibrary;
 
 namespace ImageCropperWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ImageProcessor _imageProcessor = new();
+        private readonly RectangleArea _rectangleArea = new(200, 200, 16.0 / 9.0);
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _imageProcessor.LoadImage(openFileDialog.FileName);
+                LoadedImage.Source = new System.Windows.Media.Imaging.BitmapImage(new System.Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void RotateLeft_Click(object sender, RoutedEventArgs e) => _imageProcessor.RotateImage(-90);
+        private void RotateRight_Click(object sender, RoutedEventArgs e) => _imageProcessor.RotateImage(90);
+        private void ZoomIn_Click(object sender, RoutedEventArgs e) => _imageProcessor.ZoomImage(_imageProcessor.Zoom + 0.1);
+        private void ZoomOut_Click(object sender, RoutedEventArgs e) => _imageProcessor.ZoomImage(_imageProcessor.Zoom - 0.1);
+        private void SaveCropped_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                _imageProcessor.SaveCroppedImage(_rectangleArea, saveFileDialog.FileName);
+            }
         }
     }
 }
